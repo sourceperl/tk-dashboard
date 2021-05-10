64047f54af2bd346ea40deb0881ead8f239aa09e
sourceperl <loic.celine@free.fr>
2021-05-10 18:15:04 +0200

    fix img size and functions names

--- docker/dash-import-app/app.py
+++ docker/dash-import-app/app.py
@@ -142,24 +142,6 @@ def air_quality_atmo_hdf_job():
         logging.error(traceback.format_exc())
 
 
-def gmap_traffic_img_job():
-    # http request
-    try:
-        r = requests.get(gmap_img_url, stream=True, timeout=5.0)
-        if r.status_code == 200:
-            # convert RAW img format (bytes) to Pillow image
-            pil_img = PIL.Image.open(io.BytesIO(r.raw.read()))
-            # resize to 632x328 and force png format
-            pil_img.thumbnail([632, 328])
-            img_io = io.BytesIO()
-            pil_img.save(img_io, format='PNG')
-            # store RAW PNG to redis key
-            DB.master.set_bytes('img:traffic-map:png', img_io.getvalue())
-            DB.master.set_ttl('img:traffic-map:png', 2 * 3600)
-    except Exception:
-        logging.error(traceback.format_exc())
-
-
 def gsheet_job():
     # https request
     try:
@@ -172,6 +154,75 @@ def gsheet_job():
         redis_d = dict(update=datetime.now().isoformat('T'), tags=d)
         DB.master.set_to_json('gsheet:grt', redis_d)
         DB.master.set_ttl('gsheet:grt', ttl=2 * 3600)
+    except Exception:
+        logging.error(traceback.format_exc())
+
+
+def img_gmap_traffic_job():
+    # http request
+    try:
+        r = requests.get(gmap_img_url, stream=True, timeout=5.0)
+        if r.status_code == 200:
+            # convert RAW img format (bytes) to Pillow image
+            pil_img = PIL.Image.open(io.BytesIO(r.raw.read()))
+            # crop image
+            pil_img = pil_img.crop((0, 0, 560, 328))
+            #pil_img.thumbnail([632, 328])
+            img_io = io.BytesIO()
+            pil_img.save(img_io, format='PNG')
+            # store RAW PNG to redis key
+            DB.master.set_bytes('img:traffic-map:png', img_io.getvalue())
+            DB.master.set_ttl('img:traffic-map:png', 2 * 3600)
+    except Exception:
+        logging.error(traceback.format_exc())
+
+
+def img_grt_tw_cloud_job():
+    def is_camelcase(s):
+        return s != s.lower() and '_' not in s
+
+    try:
+        # params
+        tw_query = 'grtgaz exclude:retweets exclude:replies'
+        tw_count = 100
+        tw_oauth = OAuth1(tw_api_key, tw_api_secret, tw_access_token, tw_access_token_secret)
+        # build url
+        url = 'https://api.twitter.com/1.1/search/tweets.json?'
+        url += 'q=%s&count=%i&result_type=recent&tweet_mode=extended'
+        url %= (urllib.parse.quote(tw_query), tw_count)
+        # do request
+        r = requests.get(url, auth=tw_oauth, timeout=5.0)
+        # check error
+        if r.status_code == 200:
+            d_tweets = r.json()
+            d_hash_camel = {}
+            c_hash = Counter()
+            for tw in d_tweets['statuses']:
+                tw_msg = tw['full_text']
+                # search hashtag and count it
+                for hashtag in re.findall(r'#(\w+)', tw_msg):
+                    h_key = hashtag.lower()
+                    if is_camelcase(hashtag):
+                        d_hash_camel[h_key] = hashtag
+                    elif h_key not in d_hash_camel:
+                        d_hash_camel[h_key] = h_key
+                    c_hash.update([h_key])
+
+            # build WordCloud
+            if c_hash:
+                # build frequencies dict for generate step
+                d_freq = {}
+                for h_key, score in c_hash.most_common(25):
+                    d_freq[d_hash_camel[h_key]] = score
+                # generate a word cloud image
+                word_cloud = WordCloud(margin=5, width=327, height=226)
+                word_cloud.generate_from_frequencies(frequencies=d_freq)
+                img_io = io.BytesIO()
+                pil_img = word_cloud.to_image()
+                pil_img.save(img_io, format='PNG')
+                # store RAW PNG to redis key
+                DB.master.set_bytes('img:grt-tweet-wordcloud:png', img_io.getvalue())
+                DB.master.set_ttl('img:grt-tweet-wordcloud:png', 2 * 3600)
     except Exception:
         logging.error(traceback.format_exc())
 
@@ -262,56 +313,6 @@ def twitter_job():
             d_redis = dict(tweets=tweets_l, update=datetime.now().isoformat('T'))
             DB.master.set_to_json('twitter:tweets:grtgaz', d_redis)
             DB.master.set_ttl('twitter:tweets:grtgaz', ttl=3600)
-    except Exception:
-        logging.error(traceback.format_exc())
-
-
-def twitter2cloud_job():
-    def is_camelcase(s):
-        return s != s.lower() and '_' not in s
-
-    try:
-        # params
-        tw_query = 'grtgaz exclude:retweets exclude:replies'
-        tw_count = 100
-        tw_oauth = OAuth1(tw_api_key, tw_api_secret, tw_access_token, tw_access_token_secret)
-        # build url
-        url = 'https://api.twitter.com/1.1/search/tweets.json?'
-        url += 'q=%s&count=%i&result_type=recent&tweet_mode=extended'
-        url %= (urllib.parse.quote(tw_query), tw_count)
-        # do request
-        r = requests.get(url, auth=tw_oauth, timeout=5.0)
-        # check error
-        if r.status_code == 200:
-            d_tweets = r.json()
-            d_hash_camel = {}
-            c_hash = Counter()
-            for tw in d_tweets['statuses']:
-                tw_msg = tw['full_text']
-                # search hashtag and count it
-                for hashtag in re.findall(r'#(\w+)', tw_msg):
-                    h_key = hashtag.lower()
-                    if is_camelcase(hashtag):
-                        d_hash_camel[h_key] = hashtag
-                    elif h_key not in d_hash_camel:
-                        d_hash_camel[h_key] = h_key
-                    c_hash.update([h_key])
-
-            # build WordCloud
-            if c_hash:
-                # build frequencies dict for generate step
-                d_freq = {}
-                for h_key, score in c_hash.most_common(25):
-                    d_freq[d_hash_camel[h_key]] = score
-                # generate a word cloud image
-                word_cloud = WordCloud(margin=5, width=327, height=226)
-                word_cloud.generate_from_frequencies(frequencies=d_freq)
-                img_io = io.BytesIO()
-                pil_img = word_cloud.to_image()
-                pil_img.save(img_io, format='PNG')
-                # store RAW PNG to redis key
-                DB.master.set_bytes('img:grt-tweet-wordcloud:png', img_io.getvalue())
-                DB.master.set_ttl('img:grt-tweet-wordcloud:png', 2 * 3600)
     except Exception:
         logging.error(traceback.format_exc())
 
@@ -410,22 +411,22 @@ if __name__ == '__main__':
 
     # init scheduler
     schedule.every(60).minutes.do(air_quality_atmo_hdf_job)
-    schedule.every(2).minutes.do(gmap_traffic_img_job)
     schedule.every(5).minutes.do(gsheet_job)
+    schedule.every(2).minutes.do(img_gmap_traffic_job)
+    schedule.every(30).minutes.do(img_grt_tw_cloud_job)
     schedule.every(5).minutes.do(local_info_job)
     schedule.every(15).minutes.do(openweathermap_forecast_job)
     schedule.every(5).minutes.do(twitter_job)
-    schedule.every(30).minutes.do(twitter2cloud_job)
     schedule.every(5).minutes.do(vigilance_job)
     schedule.every(5).minutes.do(weather_today_job)
     # first call
     air_quality_atmo_hdf_job()
-    gmap_traffic_img_job()
     gsheet_job()
+    img_gmap_traffic_job()
+    img_grt_tw_cloud_job()
     local_info_job()
     openweathermap_forecast_job()
     twitter_job()
-    twitter2cloud_job()
     vigilance_job()
     weather_today_job()
 
