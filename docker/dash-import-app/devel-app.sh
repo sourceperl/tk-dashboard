9684109ecc924aa90db9fc28655d6c6aa1fdd42a
sourceperl <loic.celine@free.fr>
2021-05-13 17:36:59 +0200

    improve devel script

--- docker/dash-import-app/devel-app.sh
+++ docker/dash-import-app/devel-app.sh
@@ -3,9 +3,10 @@
 IMG="dash-import-app-img"
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-echo "run app.py from \"${HERE}\" in container based on \"${IMG}\" image"
+echo "/usr/src/app is link to ${HERE}"
+echo "run \"python3 app.py\" to test app.py update with ${IMG} docker image"
 docker run -it --rm \
            --net dash-net \
            -v dash-conf-vol:/data/dashboard-conf-vol \
            -v ${HERE}:/usr/src/app \
-           ${IMG} python3 app.py
+           ${IMG} bash
